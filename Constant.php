b237386b9657a433566c3281cb59c5359bda78f4
hilal-1991 <62143521+hilal-1991@users.noreply.github.com>
2020-08-05 12:54:20 +0500

    Create Constant.php

---
+++ Constant.php
@@ -0,0 +1,18 @@
+Create Contant file in below directory
+app/Constants/Constant.php
+
+<?php
+
+namespace App\Constants;
+
+
+class Constant
+{
+
+
+    /* MixPanel */
+    const MIXPANEL= "MIXPANEL";
+    const MIXPANELPROJECTKEY = "PROJECT_KEY";
+    const MIXPANELVAMDELETEUSEREVENTLOG = "VAP_DeleteUserLog";
+
+}
